ea3c42a70b6eac0900bb32159d7908b6d9ac9842
Lukas Sailer <lukas.sailer@meisterplan.com>
2023-03-06 13:20:18 +0100

    Add JOOQ code generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
+import nu.studer.gradle.jooq.JooqEdition
+
 plugins {
     kotlin("jvm") version "1.8.0"
     application
-}
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+    id("nu.studer.jooq") version "8.1"
+}
 
 repositories {
     mavenCentral()
@@ -12,16 +13,49 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    implementation("org.postgresql:postgresql:42.2.27")
+    implementation("org.jooq:jooq:3.17.6")
+    jooqGenerator("org.postgresql:postgresql:42.2.27")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-kotlin {
-    jvmToolchain(8)
-}
-
 application {
     mainClass.set("MainKt")
+}
+
+jooq {
+    version.set("3.17.6")
+    edition.set(JooqEdition.OSS)
+
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+                logging = org.jooq.meta.jaxb.Logging.DEBUG
+
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/byos"
+                    user = "postgres"
+                    password = ""
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.JavaGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                        includes = ".*"
+                        excludes = ""
+                    }
+                    target.apply {
+                        packageName = "db.jooq.generated"
+                        directory = "${project.projectDir}/src/generated/java/jooq"
+                    }
+                }
+            }
+        }
+    }
 }
