a3384fe4bed80c47797bfdc6c4d36014868c5e45
Lukas Sailer <lukas.sailer@meisterplan.com>
2023-03-08 16:46:37 +0100

    Add spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
 import nu.studer.gradle.jooq.JooqEdition
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.0"
     application
 
     id("nu.studer.jooq") version "8.1"
+
+    id("org.springframework.boot") version "3.0.4"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("plugin.spring") version "1.7.22"
 }
+
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
@@ -17,14 +24,29 @@ dependencies {
     implementation("org.postgresql:postgresql:42.2.27")
     implementation("org.jooq:jooq:3.17.6")
     jooqGenerator("org.postgresql:postgresql:42.2.27")
+
+    implementation("org.springframework.boot:spring-boot-starter-graphql")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework:spring-webflux")
+    testImplementation("org.springframework.graphql:spring-graphql-test")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-application {
-    mainClass.set("MainKt")
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 jooq {
---
+++ src/main/kotlin/com/example/byos/ByosApplication.kt
@@ -0,0 +1,11 @@
+package com.example.byos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ByosApplication
+
+fun main(args: Array<String>) {
+    runApplication<ByosApplication>(*args)
+}
