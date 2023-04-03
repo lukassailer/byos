85a85a4a2fe68188b5eaa602f364059cd0983dcf
Lukas Sailer <lukas.sailer@meisterplan.com>
2023-04-03 11:30:26 +0200

    Add example join condition

--- src/main/kotlin/com/example/byos/Logic.kt
+++ src/main/kotlin/com/example/byos/Logic.kt
@@ -1,8 +1,11 @@
 package com.example.byos
 
+import db.jooq.generated.Tables.AUTHORS
+import db.jooq.generated.Tables.BOOKS
 import graphql.language.Field
 import graphql.language.OperationDefinition
 import graphql.language.SelectionSet
+import org.jooq.Condition
 import org.jooq.Record
 import org.jooq.Select
 import org.jooq.impl.DSL
@@ -30,17 +33,20 @@ private fun getOperationTree(selectionSet: SelectionSet): List<QueryNode> =
         }
     }
 
-fun resolveTree(relation: QueryNode.Relation): Select<Record> {
+fun resolveTree(relation: QueryNode.Relation, condition: Condition = DSL.noCondition()): Select<Record> {
     val (relations, attributes) = relation.children.partition { it is QueryNode.Relation }
     val attributeNames = attributes.map { (it as QueryNode.Attribute).value }.map { DSL.field(it) }
 
+    val newCond = DSL.condition(BOOKS.AUTHORID.eq(AUTHORS.ID))
+
     val subSelects = relations.map {
         DSL.multiset(
-            resolveTree(it as QueryNode.Relation)
-        )
+            resolveTree(it as QueryNode.Relation, newCond)
+        ).`as`(it.value)
     }
 
     return DSL.select(attributeNames)
         .select(subSelects)
         .from(relation.value)
+        .where(condition)
 }
