c3f12eae40ffbc445ea2586b55031b2a35a74e8c
Lukas Sailer <lukas.sailer@meisterplan.com>
2023-04-12 10:12:05 +0200

    print queries

--- src/main/kotlin/byos/JooqHelpers.kt
+++ src/main/kotlin/byos/JooqHelpers.kt
@@ -1,9 +1,16 @@
 package byos
 
 import org.jooq.DSLContext
+import org.jooq.ExecuteContext
+import org.jooq.ExecuteListener
 import org.jooq.Formattable
 import org.jooq.JSONFormat
+import org.jooq.SQLDialect
+import org.jooq.conf.Settings
 import org.jooq.impl.DSL
+import org.jooq.impl.DefaultConfiguration
+import org.jooq.impl.DefaultExecuteListenerProvider
+import java.sql.DriverManager
 
 
 private const val userName = "postgres"
@@ -11,9 +18,11 @@ private const val password = ""
 private const val url = "jdbc:postgresql://localhost:5432/byos_schema_names"
 
 fun <T> executeJooqQuery(withDsl: (dsl: DSLContext) -> T): T {
-    return DSL.using(url, userName, password).use { ctx ->
-        withDsl(ctx)
-    }
+    val connection = DriverManager.getConnection(url, userName, password)
+    val configuration = DefaultConfiguration().set(connection).set(SQLDialect.POSTGRES)
+    configuration.set(DefaultExecuteListenerProvider(PrettyPrinter()))
+
+    return withDsl(DSL.using(configuration))
 }
 
 val GRAPHQL_FORMAT = JSONFormat()
@@ -25,4 +34,20 @@ val GRAPHQL_FORMAT = JSONFormat()
 fun Formattable.formatGraphQLResponse(): String {
     val json = this.formatJSON(GRAPHQL_FORMAT)
     return "{\"data\":" + json.substring(1, json.length - 1) + "}"
+}
+
+// print sql queries (see: https://www.jooq.org/doc/latest/manual/sql-execution/execute-listeners/)
+class PrettyPrinter : ExecuteListener {
+    override fun executeStart(ctx: ExecuteContext) {
+        val create = DSL.using(
+            ctx.dialect(),
+            Settings().withRenderFormatted(true)
+        )
+
+        if (ctx.query() != null) {
+            println(create.renderInlined(ctx.query()))
+        } else if (ctx.routine() != null) {
+            println(create.renderInlined(ctx.routine()))
+        }
+    }
 }
